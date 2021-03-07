ee1d150b0aa28a3784c79bd85c5d2011fb9ba0b3
Edward <e.arechwa@gmail.com>
2021-03-07 09:25:16 +0100

    feat: add some solution

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,13 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(array) {
+  let count = 0;
+  array.forEach((item) => {
+    item.map((item) => {
+      if (item === '^^') {
+        count++;
+      }
+    });
+  });
+  return count;
 };
--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  console.log(members);
+  if (members.length) {
+    if (members.some((item) => typeof item !== 'string')) {
+      return false;
+    } else {
+      let solution = '';
+      const newArr = members.map((item) => item.toLowerCase());
+      newArr.sort();
+      newArr.map((item) => (solution += item.trim().slice(0, 1).toUpperCase()));
+      return solution;
+    }
+  } else {
+    return false;
+  }
 };
